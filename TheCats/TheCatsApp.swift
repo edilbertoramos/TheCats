bb5d4d0db498999efb19ab9969cb7577a59020b0
Edilbreto Ramos <edilberto.ramos@icloud.com>
2025-02-05 15:10:02 -0400

    Initial Commit

---
+++ TheCats/TheCatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCatsApp.swift
+//  TheCats
+//
+//  Created by Edilberto Ramos on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TheCatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
